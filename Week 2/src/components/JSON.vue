9c8dbfbc2e44343bf875eda15fb8e1eedad9a1ef
Yiheng.Cai <ycai0085@student.monash.edu>
2025-08-08 23:15:47 +1000

    Activity 13

--- Week 2/src/components/JSON.vue
+++ Week 2/src/components/JSON.vue
@@ -102,10 +102,11 @@
       <h2>v-if & v-else</h2>
       <p>Toggle visibility based on a condition.</p>
       <!-- Activity 13: Toggle the message visibility when the button is clicked. -->
-      <!-- TODO: CODE TO TOGGLE MESSAGE VISIBILITY HERE. Hint: Use the v-if directive. -->
       <button @click="showMessage = !showMessage">Toggle Message</button>
-      <p class="message success">✨ You're a Vue superstar! ✨</p>
-      <p>Click the button to see a message.</p>
+      <p v-if="showMessage" class="message success">
+        ✨ You're a Vue superstar! ✨
+      </p>
+      <p v-else>Click the button to see a message.</p>
     </section>
 
     <section class="lab-section">
