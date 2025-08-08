0ac0984f92d61032ed68f76f750e40eb8c9baaf5
Yiheng.Cai <ycai0085@student.monash.edu>
2025-08-08 21:49:12 +1000

    Task 1.2

--- Week 1/src/App.vue
+++ Week 1/src/App.vue
@@ -1,6 +1,7 @@
 <script setup>
 import HelloWorld from './components/HelloWorld.vue'
 import TheWelcome from './components/TheWelcome.vue'
+import MyName from './components/MyName.vue'
 </script>
 
 <template>
@@ -8,6 +9,7 @@ import TheWelcome from './components/TheWelcome.vue'
     <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
+      <MyName />
       <HelloWorld msg="You did it!" />
     </div>
   </header>
---
+++ Week 1/src/components/MyName.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="my-name">
+    <h1>Yiheng</h1>
+  </div>
+</template>
+
+<script setup>
+</script>
+
+<style scoped>
+h1 {
+  font-weight: 500;
+  font-size: 2.6rem;
+  position: relative;
+  top: -10px;
+}
+
+.my-name h1 {
+  text-align: center;
+}
+
+@media (min-width: 1024px) {
+  .my-name h1 {
+    text-align: left;
+  }
+}
+
+</style>
+
